d5128e4818da09e09315c742b0c74d194b314854
OsmanSujoy <usmansujoy33@gmail.com>
2023-02-20 17:54:42 +0600

    feat: added initial hearder component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex items-center">
+      <Head>
+        <title>Google Docs Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  );
 }
