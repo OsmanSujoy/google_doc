c6664004d74a2320e91bfbe7865a8f1f076d5c73
OsmanSujoy <usmansujoy33@gmail.com>
2023-03-06 10:49:26 +0600

    test: testing tinymce features

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -6,18 +6,15 @@ import {
   setDoc,
 } from 'firebase/firestore';
 import { Session } from 'next-auth';
-import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useDocumentOnce } from 'react-firebase-hooks/firestore';
-import { db } from '../firebase.config';
 
-const Editor = dynamic(
-  () => import('react-draft-wysiwyg').then((module) => module.Editor),
-  {
-    ssr: false,
-  }
-);
+import { db } from '../firebase.config';
+import CustomEditor from './CustomEditor';
+
+// const CustomSunEditor = dynamic(() => import('./SunEditor'), {
+//   ssr: false,
+// });
 interface Props {
   session: Session | null;
   id: string | string[] | undefined;
@@ -55,15 +52,11 @@ function TextEditor({ session, id, docRef }: Props) {
       { merge: true }
     );
   };
+
   return (
-    <div className=" justify-center bg-[#F8F9FA] min-h-screen pb-16">
-      <Editor
-        editorState={editorState}
-        onEditorStateChange={onEditorStateChange}
-        toolbarClassName=" flex sticky top-0 z-50 !justify-center mx-auto"
-        editorClassName="mt-6 p-10 bg-white shadow-lg max-w-5xl mx-auto mb-12 border"
-      />
-    </div>
+    // <div className=" justify-center bg-[#F8F9FA] min-h-screen pb-16">
+    <CustomEditor />
+    // </div>
   );
 }
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   images: {
     domains: ['upload.wikimedia.org', 'lh3.googleusercontent.com'],
